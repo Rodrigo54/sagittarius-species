01baf98d791ce52abd2e346a3bc2232d1c17dbce
Rodrigo Alves <rodrigo54mix@gmail.com>
2026-07-22 23:07:53 -0300

    refactor: remove obsolete portrait and room processing scripts

--- scripts/processPortraits.ts
+++
@@ -1,8 +0,0 @@
-import { gerarPortraits } from './generate-portraits';
-
-const main = async () => {
-  console.log('Processando Portraits');
-  await gerarPortraits();
-};
-
-main().then(() => console.log('Fim'));
